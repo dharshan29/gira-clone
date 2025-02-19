bc629a7acc6566851c8dc9967c71aeac1f547e8b
dharshan29 <dharshangowda2000@gmail.com>
2025-02-19 22:41:10 +0530

    server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { cn } from "@/lib/utils"
+import Providers from "@/components/query-provider"
 
 import "./globals.css";
 
@@ -22,7 +23,9 @@ export default function RootLayout({
       <body
         className={cn(inter.className, "antialiased min-h-screen")}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
