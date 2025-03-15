679386eb7199aef4267c33a83210c7ec1552c3a9
dharshan29 <dharshangowda2000@gmail.com>
2025-03-15 14:42:21 +0530

    appwrite

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,19 +1,40 @@
 import { zValidator } from "@hono/zod-validator";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { Hono } from "hono";
 
+import { MemberRole } from "@/features/members/types";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID, IMAGES_BUCKET_ID } from "@/config";
+import { DATABASE_ID, WORKSPACES_ID, IMAGES_BUCKET_ID, MEMBERS_ID } from "@/config";
 
-import { createWorkspaceSchema } from "../schemas";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
+import { generateInviteCode } from "@/lib/utils";
+import { getMember } from "@/features/members/utils";
+import { error } from "console";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
+        const user = c.get("user");
         const databases = c.get("databases");
+
+        const members = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        )
+
+        if(members.total === 0){
+            return c.json({ data: { documents: [], total: 0 } })
+        }
+
+        const workspaceIds = members.documents.map((member) => member.workspaceId)
 
         const workspaces = await databases.listDocuments(
             DATABASE_ID,
             WORKSPACES_ID,
+            [
+                // Query.orderDesc("createdAt"),
+                Query.contains("$id", workspaceIds)
+            ],
         )
 
         return c.json({ data: workspaces })
@@ -53,12 +74,78 @@ const app = new Hono()
                 {
                     name,
                     userId: user.$id,
+                    imageUrl: uploadedImageUrl,
+                    inviteCode: generateInviteCode(8)
+                }
+            );
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    userId: user.$id,
+                    workspaceId: workspace.$id,
+                    role: MemberRole.ADMIN
+                }
+            )
+
+            return c.json({ data: workspace })
+        },
+    )
+    .patch(
+        "/:workspaceId",
+        sessionMiddleware,
+        zValidator("form", updateWorkspaceSchema),
+        async (c) => {
+            const databases = c.get("databases");
+            const storage = c.get("storage");
+            const user = c.get("user");
+
+            const { workspaceId } = c.req.param();
+            const { name, image } = c.req.valid("form");
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id
+            });
+
+            if(!member || member.role !== MemberRole.ADMIN){
+                return c.json({error: "Unauthorized"}, 401)
+            }
+
+            let uploadedImageUrl: string | undefined;
+
+            if(image instanceof File){
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                )
+
+                const arrayBufer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id
+                )
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBufer).toString("base64")}`;
+            } else {
+                uploadedImageUrl = image;
+            }
+
+            const workspace = await databases.updateDocument(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId,
+                {
+                    name,
                     imageUrl: uploadedImageUrl
                 }
             );
 
             return c.json({ data: workspace })
-        },
+            
+        }
     )
-
 export default app;
